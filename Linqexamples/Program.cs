e851db25fa4cf069281d135e2da58b07a3e25c91
FZoltan40 <farkasz@kkszki.hu>
2025-11-29 12:29:01 +0100

    Most expenseive game data (Max())?

--- Linqexamples/Program.cs
+++ Linqexamples/Program.cs
@@ -13,16 +13,23 @@ namespace Linqexamples
          {
              new Game{Title = "Legend of Zelda", Genre = "Rpg", ReleaseYear = 1986, Rating = 9.5, Price = 60 },
              new Game{Title = "Super Mario Bros", Genre = "Platformer", ReleaseYear = 1985, Rating = 9.2, Price = 50 },
-             new Game{Title = "Elden Ring", Genre = "Rpg", ReleaseYear = 2022, Rating = 9.8, Price = 70 },
+             new Game{Title = "Elden Ring", Genre = "Rpg", ReleaseYear = 2022, Rating = 9.8, Price = 110 },
              new Game{Title = "Stardew Valley", Genre = "Simulation", ReleaseYear = 2016, Rating = 9.0, Price = 15 },
-             new Game{Title = "Tetris", Genre = "Puzzle", ReleaseYear = 1984, Rating = 8.9, Price = 10 },
+             new Game{Title = "Tetris", Genre = "Puzzle", ReleaseYear = 1984, Rating = 8.9, Price = 110 },
          };
         static void Main(string[] args)
         {
-            //Játékok átlag árának kiszámítása
+            //Legdrágább játék adatinak a kiszűrése
+            var mostExpensiveGamePrice = games.Max(x => x.Price);
+            var mostExpensiveGame = games.Where(x => x.Price == mostExpensiveGamePrice);
+            foreach (var item in mostExpensiveGame)
+            {
+                Console.WriteLine(item.Title);
+            }
 
+            /*Játékok átlag árának kiszámítása
             var avgPrice = games.Average(x => x.Price);
-            Console.WriteLine(avgPrice);
+            Console.WriteLine(avgPrice);*/
 
             /*Lista elemeinek rendezése
             var sortedByYear = games.OrderByDescending(x => x.ReleaseYear);
